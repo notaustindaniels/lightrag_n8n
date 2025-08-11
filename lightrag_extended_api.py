baa24d876b102ac0ea118267e1c654c5fc968339
notaustindaniels <a2daniels15@gmail.com>
2025-08-11 07:38:52 -0400

    ITS WORKING

--- lightrag_extended_api.py
+++ lightrag_extended_api.py
@@ -3348,3 +3348,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     host = os.environ.get("HOST", "0.0.0.0")
     uvicorn.run(app, host=host, port=port)
+
+
+
+#IT WORKS!!!!!!!!
